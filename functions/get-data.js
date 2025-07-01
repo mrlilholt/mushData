29b6957cad70dbbf00fd03763beeaddd5a57b306
Addison <alilholt@baldwinschool.org>
2025-07-01 18:43:54 -0400

    Add function to retrieve data from GitHub CSV files

--- functions/get-data.js
+++ functions/get-data.js
@@ -1,18 +1,34 @@
-const fs   = require('fs');
-const path = require('path');
-const Papa = require('papaparse');
+// functions/get-data.js
+const { Octokit } = require('@octokit/rest');
+const Papa        = require('papaparse');
 
 exports.handler = async () => {
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+  const owner   = process.env.GITHUB_OWNER;
+  const repo    = process.env.GITHUB_REPO;
+  const branch  = process.env.GITHUB_BRANCH || 'main';
+
+  // helper to fetch & parse a CSV on GitHub
+  async function fetchCsv(pathFile) {
+    const { data: fileData } = await octokit.repos.getContent({
+      owner, repo, path: pathFile, ref: branch
+    });
+    const csvRaw = Buffer.from(fileData.content, 'base64').toString('utf8');
+    return Papa.parse(csvRaw, { header: true, dynamicTyping: true }).data;
+  }
+
   try {
-    const envCsv = fs.readFileSync(path.join(__dirname, '..', 'data', 'EnvironmentalDataSheet.csv'), 'utf8');
-    const growCsv = fs.readFileSync(path.join(__dirname, '..', 'data', 'MushroomGrowthDataSheet.csv'), 'utf8');
-    const environmental = Papa.parse(envCsv, { header: true, dynamicTyping: true }).data;
-    const growth        = Papa.parse(growCsv, { header: true, dynamicTyping: true }).data;
+    const environmental = await fetchCsv('data/EnvironmentalDataSheet.csv');
+    const growth        = await fetchCsv('data/MushroomGrowthDataSheet.csv');
+
     return {
       statusCode: 200,
       body: JSON.stringify({ environmental, growth })
     };
   } catch (err) {
-    return { statusCode: 500, body: err.message };
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
   }
 };
